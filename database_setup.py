de1aca32b89733a9a5472876ffcd7c01aecf6850
Khaled Omar <khaledeomar@gmail.com>
2018-04-18 23:32:57 -0400

    chore:adding database files

--- database_setup.py
+++ database_setup.py
@@ -6,6 +6,14 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
  
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
  
 class Category(Base):
     __tablename__ = 'category'
@@ -29,7 +37,9 @@ class Item(Base):
     description = Column(String(250))
     category_id = Column(Integer,ForeignKey('category.id'))
     category = relationship(Category) 
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
